e3154c377ea53f2e4a55df76f07adf8e301710e8
Alejandro Chavez <alejandrochavez1202@gmail.com>
2023-03-16 22:25:14 -0400

    Adding validations and configuring cors

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes');
+const {
+  errorLogger,
+  errorHandler,
+  boomErrorHandler,
+} = require('./middlewares/error.handler');
+
 const app = express();
 const port = 3000;
-
-// This allow to receive json in body
-app.use(express.json());
+const whitelist = ['http://127.0.0.1:5500', 'https://myapp.com'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed'));
+    }
+  },
+};
 
 app.get('/', (req, res) => {
   res.send('My first express server');
 });
 
+// This allow to receive json in body
+app.use(express.json());
+app.use(cors(options));
 routerApi(app);
+app.use(errorLogger);
+app.use(boomErrorHandler);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server running in http://localhost:${port}`);
--- routes/products.router.js
+++ routes/products.router.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const ProductsService = require('./../services/products.service');
+const validatorHandler = require('./../middlewares/validator.handler');
+const {
+  createProductSchema,
+  updateProductSchema,
+  getProductSchema,
+} = require('./../schemas/product.schema');
 
 const router = express.Router();
 const service = new ProductsService();
@@ -10,36 +16,58 @@ router.get('/', async (req, res) => {
   res.status(200).json(products);
 });
 
-router.get('/:id', async (req, res) => {
-  const { id } = req.params;
-  const product = await service.findOne(id);
-  res.status(200).json(product);
-});
+router.get(
+  '/:id',
+  validatorHandler(getProductSchema, 'params'),
+
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const product = await service.findOne(id);
+      res.status(200).json(product);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 // POST
-router.post('/', async (req, res) => {
-  const body = req.body;
-  const newProduct = await service.create(body);
-  res.status(201).json(newProduct);
-});
+router.post(
+  '/',
+  validatorHandler(createProductSchema, 'body'),
+  async (req, res) => {
+    const body = req.body;
+    const newProduct = await service.create(body);
+    res.status(201).json(newProduct);
+  }
+);
 
 // PATCH
-router.patch('/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const body = req.body;
-    const product = await service.update(id, body);
-    res.status(200).json({ product });
-  } catch (error) {
-    res.status(404).json({ message: error.message });
+router.patch(
+  '/:id',
+  validatorHandler(getProductSchema, 'params'),
+  validatorHandler(updateProductSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const body = req.body;
+      const product = await service.update(id, body);
+      res.status(200).json({ product });
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
 
 // DELETE
-router.delete('/:id', async (req, res) => {
-  const { id } = req.params;
-  const response = await service.delete(id);
-  res.status(200).json(response);
-});
+router.delete(
+  '/:id',
+  validatorHandler(getProductSchema, 'params'),
+  async (req, res) => {
+    const { id } = req.params;
+    const response = await service.delete(id);
+    res.status(200).json(response);
+  }
+);
 
 module.exports = router;
